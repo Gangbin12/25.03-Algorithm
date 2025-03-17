0cd03e7eb30c9f4e42bfaa7a9d4a40e948ef7593
Gangbin12 <rudqls2023@naver.com>
2025-03-17 19:48:05 +0900

    (2025.3.17) 거품 정렬

---
+++ Class2th (Brute Force)/Source.cpp
@@ -0,0 +1,35 @@
+﻿// Class2th (Brute Force).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 완전 탐색
+    // 가능한 모든 경우의 수를 탐색하면서
+    // 결과를 찾아내는 알고리즘입니다.
+
+    int password[] = { 7, 1, 2 };
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            for (int k = 0; k < 10; k++)
+            {
+                if (password[0] == i && password[1] == j && password[2] == k)
+                {
+                    cout << "Password Release : " << i << " " << j << " " << k << endl;
+
+                    break;
+                }
+            }
+        }
+    }
+
+#pragma endregion
+
+    return 0;
+}
---
+++ Class4th (Select Sort)/Source.cpp
@@ -0,0 +1,45 @@
+﻿// Class4th (Select Sort).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+#define SIZE 5
+
+using namespace std;
+
+int main()
+{
+#pragma region 선택 정렬
+    // 주어진 리스트 중에 최소값을 찾은 다음 특정한 위치에서
+    // 결과를 교체하는 방식으로 정렬하는 알고리즘입니다.
+
+    int list[SIZE] = { 6, 2, 8, 1, 0 };
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        int min = list[i];
+
+        int index = i;
+
+        for (int j = i + 1; j < SIZE; j++)
+        {
+            if (min > list[j])
+            {
+                min = list[j];
+
+                index = j;
+            }
+        }
+
+        std::swap(list[i], list[index]);
+    }
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << list[i] << " ";
+    }
+
+#pragma endregion
+
+    return 0;
+}
